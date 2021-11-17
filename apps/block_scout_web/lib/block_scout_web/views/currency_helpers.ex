c46cfb6fb034207e7f06a9ac122f7cb482d6a092
Viktor Baranov <baranov.viktor.27@gmail.com>
2021-11-17 13:45:22 +0300

    Fix format_according_to_decimals method

--- apps/block_scout_web/lib/block_scout_web/views/currency_helpers.ex
+++ apps/block_scout_web/lib/block_scout_web/views/currency_helpers.ex
@@ -42,6 +42,15 @@ defmodule BlockScoutWeb.CurrencyHelpers do
 
       iex> format_according_to_decimals(205000, Decimal.new(2))
       "2,050"
+
+      iex> format_according_to_decimals(105000, Decimal.new(0))
+      "105,000"
+
+      iex> format_according_to_decimals(105000000000000000000, Decimal.new(100500))
+      "105"
+
+      iex> format_according_to_decimals(105000000000000000000, nil)
+      "105,000,000,000,000,000,000"
   """
   @spec format_according_to_decimals(non_neg_integer() | nil, nil) :: String.t()
   def format_according_to_decimals(nil, _) do
@@ -60,9 +69,18 @@ defmodule BlockScoutWeb.CurrencyHelpers do
 
   @spec format_according_to_decimals(Decimal.t(), Decimal.t()) :: String.t()
   def format_according_to_decimals(value, decimals) do
-    value
-    |> divide_decimals(decimals)
-    |> thousands_separator()
+    cond do
+      decimals == Decimal.new(0) ->
+        value
+
+      Decimal.cmp(decimals, 18) == :gt ->
+        format_according_to_decimals(value, Decimal.new(18))
+
+      true ->
+        value
+        |> divide_decimals(decimals)
+        |> thousands_separator()
+    end
   end
 
   defp thousands_separator(value) do
