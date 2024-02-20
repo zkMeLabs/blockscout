558ec45b6a694ae31b5b1f36435d8e516a5cafcf
Viktor Baranov <baranov.viktor.27@gmail.com>
2024-02-20 19:00:20 +0300

    Refactoring, cover with tests

--- apps/ethereum_jsonrpc/lib/ethereum_jsonrpc/variant.ex
+++ apps/ethereum_jsonrpc/lib/ethereum_jsonrpc/variant.ex
@@ -115,16 +115,21 @@ defmodule EthereumJSONRPC.Variant do
     end
   end
 
+  # credo:disable-for-next-line
   defp get_default_variant do
-    case Application.get_env(:explorer, :chain_type) do
-      "polygon_zkevm" -> "geth"
-      "zetachain" -> "geth"
-      "shibarium" -> "geth"
-      "stability" -> "geth"
-      "zksync" -> "geth"
-      "rsk" -> "rsk"
-      "filecoin" -> "filecoin"
-      _ -> "nethermind"
+    if Mix.env() == :test do
+      "nethermind"
+    else
+      case Application.get_env(:explorer, :chain_type) do
+        "polygon_zkevm" -> "geth"
+        "zetachain" -> "geth"
+        "shibarium" -> "geth"
+        "stability" -> "geth"
+        "zksync" -> "geth"
+        "rsk" -> "rsk"
+        "filecoin" -> "filecoin"
+        _ -> "nethermind"
+      end
     end
   end
 end
